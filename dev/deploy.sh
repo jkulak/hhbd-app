33338d0650cc6c9bfa7fe1344cd67722bd86dd1c
Jakub Kułak <jakub.kulak@bauermedia.com>
2014-09-30 23:01:55 +0200

    Update deploy.sh script

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -1,36 +1,41 @@
+#!/usr/bin/env bash
+
 # 2012-12-16
 # Jakub Kułak
-# 
+#
 # This script is used to deploy hhbdevolution on production server
 #
-# Usage: 
+# Usage: $ ./deploy.sh "new-feature-added"
 
-APP_PATH="/home/hhbd/www/php/app/www.hhbd.pl"
+APP_PATH="/home/hhbd/www/vhosts/www.hhbd.pl"
+DATE=$(date +%F)
 
-echo "Deployment script for hhbdevolution project (from GitHub)";
+echo "Deploying hhbd.pl"
+echo "---> to: $APP_PATH";
+
 if [[ "$1" != "" ]]; then
+
+  TAG_NAME=$DATE-$1
+
   #clone read-only repo from github
-  git clone git://github.com/jkulak/hhbdevolution.git $APP_PATH/code/tags/$1
+  git clone git://github.com/jkulak/hhbd.git $APP_PATH/code/tags/$TAG_NAME
 
   # copy previous .httaccess instead of creating it from example
-  cp $APP_PATH/www/public/.htaccess $APP_PATH/code/tags/$1/public/
+  cp $APP_PATH/www/public/.htaccess $APP_PATH/code/tags/$TAG_NAME/www/hhbd.pl/public/
 
-  # set applcaition/configs/application.ini (database, baseUrl)
-  cp $APP_PATH/www/application/configs/application.ini $APP_PATH/code/tags/$1/application/configs/
-
-  # make symbolic link to Zend libraries - no need, because Zend is included in repo
-  # ln -s /home/hhbd/www/php/lib/external/Zend /home/hhbd/www/php/app/www.hhbd.pl/svn/www.hhbd.pl/tags/v0.$1/library/Zend
+  # copy previous application.ini (database, baseUrl)
+  cp $APP_PATH/www/application/configs/application.ini $APP_PATH/code/tags/$TAG_NAME/www/hhbd.pl/application/configs/
 
   # make symbolic link to image content
-  ln -s $APP_PATH/svn/content/images/ $APP_PATH/code/tags/$1/public/database
+  ln -s $APP_PATH/svn/content/images/ $APP_PATH/code/tags/$TAG_NAME/www/hhbd.pl/public/database
 
-  ln -s $APP_PATH/code/tags/$1 $APP_PATH/code/new
+  ln -s $APP_PATH/code/tags/$TAG_NAME/www/hhbd.pl $APP_PATH/code/new
   rm $APP_PATH/code/prev
   mv $APP_PATH/code/current $APP_PATH/code/prev
   mv $APP_PATH/code/new $APP_PATH/code/current
-  touch $APP_PATH/code/current/.tag-$1
+  touch $APP_PATH/code/current/.tag-$TAG_NAME
 
   ls -la $APP_PATH/www
 else
-  echo "Please specify tag number 0.xx as parameter (ie. ./deploy.sh \"16\")";
+  echo "Please give this release a name (ie. ./deploy.sh \"Add-voting\")";
 fi
