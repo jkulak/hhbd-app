3941fcc95d647781e4f0d5583de864007f7c9a1f
Jakub Kułak <jakub.kulak@bauermedia.com>
2014-10-08 21:53:13 +0200

    Update deployment scripts

--- dev/git-deploy.php
+++ dev/git-deploy.php
@@ -1,13 +1,13 @@
 <?php
-    try {
-      $payload = json_decode($_REQUEST['payload']);
-    } catch(Exception $e) {
-      exit(0);
-    }
+try {
+    $payload = json_decode($_REQUEST['payload']);
+} catch(Exception $e) {
+    exit(0);
+}
 
-    //log the request
-    file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+//log the request
+file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 
-    if ($payload->ref === 'refs/heads/master') {
-      exec('./jenkins-deploy.sh');
-    }
+if ($payload->ref === 'refs/heads/master') {
+    exec('./deploy.sh ' . $payload['head_commit']['id']);
+}
